8d2430e8835b73158cbb8151df4960f5e9019b45
dnyaneshwarithopate <dnyaneshwarithopate28@gmail.com>
2025-10-29 15:56:31 +0530

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,35 @@
+
+import type {NextConfig} from 'next';
+require('dotenv').config({ path: './.env' });
+
+const supabaseHostname = process.env.NEXT_PUBLIC_SUPABASE_URL
+  ? new URL(process.env.NEXT_PUBLIC_SUPABASE_URL).hostname
+  : undefined;
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+      ...(supabaseHostname ? [{
+          protocol: 'https',
+          hostname: supabaseHostname,
+        } as const] : []),
+    ],
+  }
+};
+
+export default nextConfig;
